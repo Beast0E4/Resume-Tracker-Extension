72c9c607323598677bfbca5ccb497d652dc5d742
Beast0E4 <ebison.04@gmail.com>
2025-12-15 22:44:29 +0530

    Feat: Added BASE URL fr=or extension

--- extension/config.js
+++ extension/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  API_BASE_URL: "http://localhost:8080/api/applications",
+  API_BASE_URL: "https://resume-tracker-2udf.onrender.com/api/applications",
 };
 
 export default CONFIG;
